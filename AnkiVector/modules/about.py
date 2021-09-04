b850d632658082db9216427d4cfc0aebc12f5ff0
Venujasadew <87709786+Venujasadew@users.noreply.github.com>
2021-09-04 21:28:11 +0530

    Initialize patch-2

---
+++ AnkiVector/modules/about.py
@@ -0,0 +1,17 @@
+__help__ = """
+** Rose Bot 🌹 - A Powerful Telegram Group Manager Bot **
+
+All Credits & Special thanks 🌞 
+
+🌐 First Owners [Yakari music](https://github.com/youtubeslgeekshow/call-music-plus-bot) [Damantha🇱🇰](https://t.me/Damantha_Jasinghe)[Daisy X](https://github.com/TeamDaisyX/DaisyX) 
+
+🌐 I added
+Force Subscribe for music play
+song downloader 
+update some customizes button and more....
+
+🌐 Join with us for more education 📖
+
+ [Updates Channel🔔 ](https://t.me/sl_bot_zone)
+"""
+__mod_name__ = "About 🇱🇰"
